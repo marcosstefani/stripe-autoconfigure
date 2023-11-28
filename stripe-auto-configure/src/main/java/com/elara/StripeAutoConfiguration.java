b5fcf677d82d9c55d49e465fd1ccd07f50fbb725
Marcos Rosa <marcos.rosa@mindera.com>
2023-11-28 15:49:34 +0000

    add configuration

--- stripe-auto-configure/src/main/java/com/elara/StripeAutoConfiguration.java
+++ stripe-auto-configure/src/main/java/com/elara/StripeAutoConfiguration.java
@@ -1,4 +1,65 @@
 package com.elara;
 
+import com.stripe.StripeClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.PasswordAuthentication;
+
+import static java.util.Objects.nonNull;
+
+@Configuration
+@EnableConfigurationProperties({StripeProperties.class})
 public class StripeAutoConfiguration {
+    private final StripeProperties properties;
+
+    public StripeAutoConfiguration(StripeProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    @Qualifier("stripeClient")
+    public StripeClient stripeClient() {
+        // basic builder
+        var builder = StripeClient.builder()
+                .setApiKey(properties.getApiKey())
+                .setMaxNetworkRetries(properties.getMaxNetworkRetries())
+                .setConnectTimeout(properties.getConnectTimeoutInSeconds() * 1000)
+                .setReadTimeout(properties.getReadTimeoutInSeconds() * 1000);
+
+        // add optional proxyCredential
+        final var proxyCredential = properties.getProxyCredential();
+        if (nonNull(proxyCredential)) {
+            var authentication = new PasswordAuthentication(
+                    proxyCredential.getUsername(),
+                    proxyCredential.getPassword().toCharArray()
+            );
+            builder.setProxyCredential(authentication);
+        }
+
+        // add optional clientId
+        if (nonNull(properties.getClientId()) && !properties.getClientId().isBlank()) {
+            builder.setClientId(properties.getClientId());
+        }
+
+        // add optional addresses
+        var address = properties.getAddress();
+        if (nonNull(address)) {
+            if (nonNull(address.getApiBase()) && !address.getApiBase().isBlank()) {
+                builder.setApiBase(address.getApiBase());
+            }
+
+            if (nonNull(address.getConnectBase()) && !address.getConnectBase().isBlank()) {
+                builder.setConnectBase(address.getConnectBase());
+            }
+
+            if (nonNull(address.getFilesBase()) && !address.getFilesBase().isBlank()) {
+                builder.setFilesBase(address.getFilesBase());
+            }
+        }
+
+        return builder.build();
+    }
 }
--- stripe-auto-configure/src/main/java/com/elara/StripeProperties.java
+++ stripe-auto-configure/src/main/java/com/elara/StripeProperties.java
@@ -1,4 +1,121 @@
 package com.elara;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("stripe")
 public class StripeProperties {
+    private String apiKey;
+    private String clientId;
+    private int maxNetworkRetries = 2;
+    private int connectTimeoutInSeconds = 30;
+    private int readTimeoutInSeconds = 80;
+    private ProxyCredential proxyCredential;
+    private Address address;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getMaxNetworkRetries() {
+        return maxNetworkRetries;
+    }
+
+    public void setMaxNetworkRetries(int maxNetworkRetries) {
+        this.maxNetworkRetries = maxNetworkRetries;
+    }
+
+    public int getConnectTimeoutInSeconds() {
+        return connectTimeoutInSeconds;
+    }
+
+    public void setConnectTimeoutInSeconds(int connectTimeoutInSeconds) {
+        this.connectTimeoutInSeconds = connectTimeoutInSeconds;
+    }
+
+    public int getReadTimeoutInSeconds() {
+        return readTimeoutInSeconds;
+    }
+
+    public void setReadTimeoutInSeconds(int readTimeoutInSeconds) {
+        this.readTimeoutInSeconds = readTimeoutInSeconds;
+    }
+
+    public ProxyCredential getProxyCredential() {
+        return proxyCredential;
+    }
+
+    public void setProxyCredential(ProxyCredential proxyCredential) {
+        this.proxyCredential = proxyCredential;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public static class ProxyCredential {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    public static class Address {
+        private String apiBase;
+        private String connectBase;
+        private String filesBase;
+
+        public String getApiBase() {
+            return apiBase;
+        }
+
+        public void setApiBase(String apiBase) {
+            this.apiBase = apiBase;
+        }
+
+        public String getConnectBase() {
+            return connectBase;
+        }
+
+        public void setConnectBase(String connectBase) {
+            this.connectBase = connectBase;
+        }
+
+        public String getFilesBase() {
+            return filesBase;
+        }
+
+        public void setFilesBase(String filesBase) {
+            this.filesBase = filesBase;
+        }
+    }
 }
